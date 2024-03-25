32b3f5ceaaa713923cdddd00c5d292e496e2cad8
OlenaHlu <glushenkova_es@ukr.net>
2024-03-25 22:15:48 +0200

    hw11-1

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,27 @@
+import iziToast from "izitoast";
+import "izitoast/dist/css/iziToast.min.css";
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+export function getImage() {
+    const BASE_URL = 'https://pixabay.com';
+    const END_POINT = '/api/';
+    const params = new URLSearchParams({
+        key: '43066959-f9f55707df0fe34b818b99119',
+        q: value,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: 'true',
+    });
+    const url = `${BASE_URL}${END_POINT}?${params}`;
+
+
+    return fetch(url).then(response => {
+        if (!response.ok) {
+            throw new Error(response.status);
+        }
+
+        return response.json();
+    });
+}
